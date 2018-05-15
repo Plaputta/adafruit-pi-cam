8d467a3f5c28d58a24eea973d30dfdfba6281db7
tplaputta <tobias.plaputta@affinitas.de>
2018-05-15 18:52:54 +0200

    Preparing for p3

--- cam.py
+++ cam.py
@@ -1,29 +1,4 @@
-# Point-and-shoot camera for Raspberry Pi w/camera and Adafruit PiTFT.
-# This must run as root (sudo python cam.py) due to framebuffer, etc.
-#
-# Adafruit invests time and resources providing this open source code, 
-# please support Adafruit and open-source development by purchasing 
-# products from Adafruit, thanks!
-#
-# http://www.adafruit.com/products/998  (Raspberry Pi Model B)
-# http://www.adafruit.com/products/1367 (Raspberry Pi Camera Board)
-# http://www.adafruit.com/products/1601 (PiTFT Mini Kit)
-# This can also work with the Model A board and/or the Pi NoIR camera.
-#
-# Prerequisite tutorials: aside from the basic Raspbian setup and
-# enabling the camera in raspi-config, you should configure WiFi (if
-# using wireless with the Dropbox upload feature) and read these:
-# PiTFT setup (the tactile switch buttons are not required for this
-# project, but can be installed if you want them for other things):
-# http://learn.adafruit.com/adafruit-pitft-28-inch-resistive-touchscreen-display-raspberry-pi
-# Dropbox setup (if using the Dropbox upload feature):
-# http://raspi.tv/2013/how-to-use-dropbox-with-raspberry-pi
-#
-# Written by Phil Burgess / Paint Your Dragon for Adafruit Industries.
-# BSD license, all text above must be included in any redistribution.
-
 import atexit
-import pickle
 import errno
 import fnmatch
 import io
@@ -39,20 +14,6 @@ import ft5406
 from pygame.locals import *
 from subprocess import call
 
-
-
-
-# UI classes ---------------------------------------------------------------
-
-# Small resistive touchscreen is best suited to simple tap interactions.
-# Importing a big widget library seemed a bit overkill.  Instead, a couple
-# of rudimentary classes are sufficient for the UI elements:
-
-# Icon is a very simple bitmap class, just associates a name and a pygame
-# image (PNG loaded from icons directory) for each.
-# There isn't a globally-declared fixed list of Icons.  Instead, the list
-# is populated at runtime from the contents of the 'icons' directory.
-
 class Icon:
 
 	def __init__(self, name):
@@ -61,22 +22,6 @@ class Icon:
 	    self.bitmap = pygame.image.load(iconPath + '/' + name + '.png')
 	  except:
 	    pass
-
-# Button is a simple tappable screen region.  Each has:
-#  - bounding rect ((X,Y,W,H) in pixels)
-#  - optional background color and/or Icon (or None), always centered
-#  - optional foreground Icon, always centered
-#  - optional single callback function
-#  - optional single value passed to callback
-# Occasionally Buttons are used as a convenience for positioning Icons
-# but the taps are ignored.  Stacking order is important; when Buttons
-# overlap, lowest/first Button in list takes precedence when processing
-# input, and highest/last Button is drawn atop prior Button(s).  This is
-# used, for example, to center an Icon by creating a passive Button the
-# width of the full screen, but with other buttons left or right that
-# may take input precedence (e.g. the Effect labels & buttons).
-# After Icons are loaded at runtime, a pass is made through the global
-# buttons[] list to assign the Icon objects (from names) to each Button.
 
 class Button:
 
@@ -130,28 +75,11 @@ class Button:
 	        self.iconBg = i
 	        break
 
-
-# UI callbacks -------------------------------------------------------------
-# These are defined before globals because they're referenced by items in
-# the global buttons[] list.
-
-def isoCallback(n): # Pass 1 (next ISO) or -1 (prev ISO)
-	global isoMode
-	setIsoMode((isoMode + n) % len(isoData))
-
 def settingCallback(n): # Pass 1 (next setting) or -1 (prev setting)
 	global screenMode
 	screenMode += n
 	if screenMode < 4:               screenMode = len(buttons) - 1
 	elif screenMode >= len(buttons): screenMode = 4
-
-def fxCallback(n): # Pass 1 (next effect) or -1 (prev effect)
-	global fxMode
-	setFxMode((fxMode + n) % len(fxData))
-
-def quitCallback(): # Quit confirmation button
-	saveSettings()
-	raise SystemExit
 
 def viewCallback(n): # Viewfinder buttons
 	global loadIdx, scaled, screenMode, screenModePrior, settingMode, storeMode
@@ -174,7 +102,6 @@ def doneCallback(): # Exit settings
 	global screenMode, settingMode
 	if screenMode > 3:
 	  settingMode = screenMode
-	  saveSettings()
 	screenMode = 3 # Switch back to viewfinder mode
 
 def imageCallback(n): # Pass 1 (next image), -1 (prev image) or 0 (delete)
@@ -199,21 +126,6 @@ def deleteCallback(n): # Delete confirmation
 	    scaled     = None
 	    loadIdx    = -1
 
-def storeModeCallback(n): # Radio buttons on storage settings screen
-	global storeMode
-	buttons[4][storeMode + 3].setBg('radio3-0')
-	storeMode = n
-	buttons[4][storeMode + 3].setBg('radio3-1')
-
-def sizeModeCallback(n): # Radio buttons on size settings screen
-	global sizeMode
-	buttons[5][sizeMode + 3].setBg('radio3-0')
-	sizeMode = n
-	buttons[5][sizeMode + 3].setBg('radio3-1')
-	camera.resolution = sizeData[sizeMode][1]
-#	camera.crop       = sizeData[sizeMode][2]
-
-
 # Global stuff -------------------------------------------------------------
 
 screenMode      =  3      # Current screen mode; default = viewfinder
@@ -229,35 +141,11 @@ saveIdx         = -1      # Image index for saving (-1 = none set yet)
 loadIdx         = -1      # Image index for loading
 scaled          = None    # pygame Surface w/last-loaded image
 
-# To use Dropbox uploader, must have previously run the dropbox_uploader.sh
-# script to set up the app key and such.  If this was done as the normal pi
-# user, set upconfig to the .dropbox_uploader config file in that account's
-# home directory.  Alternately, could run the setup script as root and
-# delete the upconfig line below.
-uploader        = '/home/pi/Dropbox-Uploader/dropbox_uploader.sh'
-upconfig        = '/home/pi/.dropbox_uploader'
-
 sizeData = [ # Camera parameters for different size settings
  # Full res      Viewfinder  Crop window
  [(2592, 1944), (800, 480), (0.0   , 0.0   , 1.0   , 1.0   )], # Large
  [(1920, 1080), (800, 480), (0.1296, 0.2222, 0.7408, 0.5556)], # Med
  [(1440, 1080), (800, 480), (0.2222, 0.2222, 0.5556, 0.5556)]] # Small
-
-isoData = [ # Values for ISO settings [ISO value, indicator X position]
- [  0,  27], [100,  64], [200,  97], [320, 137],
- [400, 164], [500, 197], [640, 244], [800, 297]]
-
-# A fixed list of image effects is used (rather than polling
-# camera.IMAGE_EFFECTS) because the latter contains a few elements
-# that aren't valid (at least in video_port mode) -- e.g. blackboard,
-# whiteboard, posterize (but posterise, British spelling, is OK).
-# Others have no visible effect (or might require setting add'l
-# camera parameters for which there's no GUI yet) -- e.g. saturation,
-# colorbalance, colorpoint.
-fxData = [
-  'none', 'sketch', 'gpen', 'pastel', 'watercolor', 'oilpaint', 'hatch',
-  'negative', 'colorswap', 'posterise', 'denoise', 'blur', 'film',
-  'washedout', 'emboss', 'cartoon', 'solarize' ]
 
 pathData = [
   '/home/pi/Photos',     # Path for storeMode = 0 (Photos folder)
@@ -276,12 +164,12 @@ icons = [] # This list gets populated at startup
 
 buttons = [
   # Screen mode 0 is photo playback
-  [Button((  0,188,320, 52), bg='done' , cb=doneCallback),
+  [Button((  0,428,400, 52), bg='done' , cb=doneCallback),
    Button((  0,  0, 80, 52), bg='prev' , cb=imageCallback, value=-1),
-   Button((240,  0, 80, 52), bg='next' , cb=imageCallback, value= 1),
+   Button((720,  0, 80, 52), bg='next' , cb=imageCallback, value= 1),
    Button(( 88, 70,157,102)), # 'Working' label (when enabled)
    Button((148,129, 22, 22)), # Spinner (when enabled)
-   Button((121,  0, 78, 52), bg='trash', cb=imageCallback, value= 0)],
+   Button((361,  0, 78, 52), bg='trash', cb=imageCallback, value= 0)],
 
   # Screen mode 1 is delete confirmation
   [Button((  0,35,320, 33), bg='delete'),
@@ -304,50 +192,6 @@ buttons = [
 
   # Remaining screens are settings modes
 
-  # Screen mode 4 is storage settings
-  [Button((  0,188,320, 52), bg='done', cb=doneCallback),
-   Button((  0,  0, 80, 52), bg='prev', cb=settingCallback, value=-1),
-   Button((240,  0, 80, 52), bg='next', cb=settingCallback, value= 1),
-   Button((  2, 60,100,120), bg='radio3-1', fg='store-folder',
-    cb=storeModeCallback, value=0),
-   Button((110, 60,100,120), bg='radio3-0', fg='store-boot',
-    cb=storeModeCallback, value=1),
-   Button((218, 60,100,120), bg='radio3-0', fg='store-dropbox',
-    cb=storeModeCallback, value=2),
-   Button((  0, 10,320, 35), bg='storage')],
-
-  # Screen mode 5 is size settings
-  [Button((  0,188,320, 52), bg='done', cb=doneCallback),
-   Button((  0,  0, 80, 52), bg='prev', cb=settingCallback, value=-1),
-   Button((240,  0, 80, 52), bg='next', cb=settingCallback, value= 1),
-   Button((  2, 60,100,120), bg='radio3-1', fg='size-l',
-    cb=sizeModeCallback, value=0),
-   Button((110, 60,100,120), bg='radio3-0', fg='size-m',
-    cb=sizeModeCallback, value=1),
-   Button((218, 60,100,120), bg='radio3-0', fg='size-s',
-    cb=sizeModeCallback, value=2),
-   Button((  0, 10,320, 29), bg='size')],
-
-  # Screen mode 6 is graphic effect
-  [Button((  0,188,320, 52), bg='done', cb=doneCallback),
-   Button((  0,  0, 80, 52), bg='prev', cb=settingCallback, value=-1),
-   Button((240,  0, 80, 52), bg='next', cb=settingCallback, value= 1),
-   Button((  0, 70, 80, 52), bg='prev', cb=fxCallback     , value=-1),
-   Button((240, 70, 80, 52), bg='next', cb=fxCallback     , value= 1),
-   Button((  0, 67,320, 91), bg='fx-none'),
-   Button((  0, 11,320, 29), bg='fx')],
-
-  # Screen mode 7 is ISO
-  [Button((  0,188,320, 52), bg='done', cb=doneCallback),
-   Button((  0,  0, 80, 52), bg='prev', cb=settingCallback, value=-1),
-   Button((240,  0, 80, 52), bg='next', cb=settingCallback, value= 1),
-   Button((  0, 70, 80, 52), bg='prev', cb=isoCallback    , value=-1),
-   Button((240, 70, 80, 52), bg='next', cb=isoCallback    , value= 1),
-   Button((  0, 79,320, 33), bg='iso-0'),
-   Button((  9,134,302, 26), bg='iso-bar'),
-   Button(( 17,157, 21, 19), bg='iso-arrow'),
-   Button((  0, 10,320, 29), bg='iso')],
-
   # Screen mode 8 is quit confirmation
   [Button((  0,188,320, 52), bg='done'   , cb=doneCallback),
    Button((  0,  0, 80, 52), bg='prev'   , cb=settingCallback, value=-1),
@@ -359,45 +203,6 @@ buttons = [
 
 # Assorted utility functions -----------------------------------------------
 
-def setFxMode(n):
-	global fxMode
-	fxMode = n
-	camera.image_effect = fxData[fxMode]
-	buttons[6][5].setBg('fx-' + fxData[fxMode])
-
-def setIsoMode(n):
-	global isoMode
-	isoMode    = n
-	camera.ISO = isoData[isoMode][0]
-	buttons[7][5].setBg('iso-' + str(isoData[isoMode][0]))
-	buttons[7][7].rect = ((isoData[isoMode][1] - 10,) +
-	  buttons[7][7].rect[1:])
-
-def saveSettings():
-	try:
-	  outfile = open('cam.pkl', 'wb')
-	  # Use a dictionary (rather than pickling 'raw' values) so
-	  # the number & order of things can change without breaking.
-	  d = { 'fx'    : fxMode,
-	        'iso'   : isoMode,
-	        'size'  : sizeMode,
-	        'store' : storeMode }
-	  pickle.dump(d, outfile)
-	  outfile.close()
-	except:
-	  pass
-
-def loadSettings():
-	try:
-	  infile = open('cam.pkl', 'rb')
-	  d      = pickle.load(infile)
-	  infile.close()
-	  if 'fx'    in d: setFxMode(   d['fx'])
-	  if 'iso'   in d: setIsoMode(  d['iso'])
-	  if 'size'  in d: sizeModeCallback( d['size'])
-	  if 'store' in d: storeModeCallback(d['store'])
-	except:
-	  pass
 
 # Scan files in a directory, locating JPEGs with names matching the
 # software's convention (IMG_XXXX.JPG), returning a tuple with the
@@ -599,8 +404,6 @@ for s in buttons:        # For each screenful of buttons...
       if b.fg == i.name:
         b.iconFg = i
         b.fg     = None
-
-loadSettings() # Must come last; fiddles with Button/Icon states
 
 
 # Main loop ----------------------------------------------------------------
