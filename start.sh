37d036fd7bf22883116c39cc3129de8cab506383
tplaputta <tobias.plaputta@affinitas.de>
2018-05-17 12:50:29 +0200

    Preparing for p3

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [ -z "$STY" ]; then exec screen -dm -S gruntScreen /bin/bash "$0"; fi
+cd /home/pi/photobooth/
+
+while true; do
+    echo "Running Hochzeitsblitzer in 3s..."
+    sleep 3
+    python3 cam.py
+done
